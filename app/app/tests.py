ef0aced48eb4d3815a1251e1712676b5b825258b
mortaza samadpoor <morysam96@gmail.com>
2021-07-10 19:38:14 +0430

    unit test with TDD

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .calc import add
+from .calc import add,subtract
 
 class CalcTest(TestCase):
     # first word function should was "test"
@@ -7,5 +7,12 @@ class CalcTest(TestCase):
         """Test that two numbers are added together"""
         self.assertEqual(add(3,8),11)
 
+    #write test before you write the code(unit test with TDD)
+    def test_subtract_numbers(self):
+        """Test that values are subtracted and returned"""
+        self.assertEqual(subtract(5,11),6)
 
-#terminal:docker-compose run app sh -c "python manage.py test"
+
+
+
+#terminal:docker-compose run app sh -c "python manage.py test && flake8"
