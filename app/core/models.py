ddf8f97118bcc1b32bf41bbdd307bb3ffc164ccd
mortaza samadpoor <morysam96@gmail.com>
2021-07-11 00:34:09 +0430

    Add validation for email field and test them

--- app/core/models.py
+++ app/core/models.py
@@ -12,6 +12,8 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new User"""
         """can't set password because has be encrypted"""
+        if not email:
+            raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         """supporting multiple databases"""
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -27,4 +27,9 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email.lower())
 
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
 # terminal:docker-compose run app sh -c "python manage.py test && flake8"
